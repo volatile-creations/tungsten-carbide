ef1a2b88fa19de2b9ee34ee4aea0b748cb531a06
Jan-Marten de Boer <github@janmarten.name>
2023-08-05 12:51:46 +0200

    Remove user repository

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Entity]
 #[ORM\Index(fields: ['emailAddress'])]
 class User
 {
