1fa644f4b05f680d165c137e230ca6ebd5be46cc
Jan-Marten de Boer <github@janmarten.name>
2023-08-06 12:57:55 +0200

    Ensure tests can run with a test database

---
+++ tests/Extension/SetupApplication.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Extension;
+
+use PHPUnit\Event\TestRunner\Started;
+use PHPUnit\Event\TestRunner\StartedSubscriber;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+final readonly class SetupApplication implements StartedSubscriber
+{
+    public function __construct(
+        private OutputInterface $output,
+        private bool $clearCache,
+        private bool $migrateDatabase,
+        private bool $loadFixtures
+    ) {
+    }
+
+    public function notify(Started $event): void
+    {
+        $this->output->write('Setting up Symfony application. . .');
+
+        if ($this->clearCache) {
+            self::run('cache:pool:clear', 'cache.global_clearer');
+            self::run('cache:clear');
+
+            if ($this->migrateDatabase) {
+                self::run('doctrine:cache:clear-metadata', '--no-debug', '--flush');
+            }
+        }
+
+        if ($this->migrateDatabase) {
+            try {
+                self::run('doctrine:database:drop', '--force');
+            } catch (ProcessFailedException) {
+                // Is allowed to fail.
+            }
+
+            self::run('doctrine:database:create');
+            self::run('doctrine:migrations:migrate');
+        }
+
+        if ($this->loadFixtures) {
+            self::run('doctrine:fixtures:load');
+        }
+
+        $this->output->writeln(' [OK]');
+    }
+
+    private static function run(string ...$arguments): void
+    {
+        if (!in_array('--no-interaction', $arguments, true)
+            || !in_array('-n', $arguments, true)
+        ) {
+            $arguments[] = '--no-interaction';
+        }
+
+        $process = new Process(command: ['bin/console', ...$arguments]);
+        $process->setWorkingDirectory(
+            realpath(__DIR__ . '/../../')
+        );
+        $process->setEnv(
+            [
+                'APP_ENV' => 'test',
+                ...$process->getEnv()
+            ]
+        );
+        $process->mustRun();
+    }
+}
---
+++ tests/Extension/SymfonyExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Extension;
+
+use PHPUnit\Runner\Extension\Extension;
+use PHPUnit\Runner\Extension\Facade;
+use PHPUnit\Runner\Extension\ParameterCollection;
+use PHPUnit\TextUI\Configuration\Configuration;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\NullOutput;
+
+final readonly class SymfonyExtension implements Extension
+{
+    public function bootstrap(
+        Configuration $configuration,
+        Facade $facade,
+        ParameterCollection $parameters
+    ): void {
+        $output = self::isEnabled($parameters, 'output')
+            ? new ConsoleOutput()
+            : new NullOutput();
+
+        $facade->registerSubscriber(
+            new SetupApplication(
+                output: $output,
+                clearCache: self::isEnabled($parameters, 'clearCache'),
+                migrateDatabase: self::isEnabled($parameters, 'migrateDatabase'),
+                loadFixtures: self::isEnabled($parameters, 'loadFixtures')
+            )
+        );
+    }
+
+    private static function isEnabled(
+        ParameterCollection $parameters,
+        string $key
+    ): bool {
+        return strtolower($parameters->get($key)) === 'true';
+    }
+}
