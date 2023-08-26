99cb637610e0f00c35c873733a948924ad2841a1
Jan-Marten de Boer <jan-marten.deboer@abbi-insights.com>
2023-08-26 09:03:53 +0200

    Remove link with domain model

--- src/DTO/User/User.php
+++ src/DTO/User/User.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\DTO\User;
 
-use App\Domain\User\User as AggregateRoot;
 use App\Domain\User\UserId;
 
 final readonly class User
@@ -13,13 +12,5 @@ final readonly class User
         public UserId $id,
         public string $emailAddress
     ) {
-    }
-
-    public static function fromUser(AggregateRoot $user): self
-    {
-        return new self(
-            id: $user->aggregateRootId(),
-            emailAddress: $user->getEmailAddress()
-        );
     }
 }
