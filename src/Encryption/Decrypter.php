416fa495ec43931f55f1315a9229a445a8088396
Jan-Marten de Boer <github@janmarten.name>
2023-12-27 16:47:53 +0100

    Revert to bare Symfony 7 project

--- src/Encryption/Decrypter.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace App\Encryption;
-
-interface Decrypter
-{
-    public function decrypt(string $encrypted): string;
-}
--- src/Encryption/Encrypter.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace App\Encryption;
-
-interface Encrypter
-{
-    public function encrypt(string $payload): string;
-}
