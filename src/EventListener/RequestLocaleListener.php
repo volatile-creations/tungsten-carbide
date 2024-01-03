73f1dea48bdd967fdd86050de5219128cb870c65
Jan-Marten de Boer <github@janmarten.name>
2024-01-03 10:27:44 +0100

    Add locale switcher

---
+++ src/EventListener/RequestLocaleListener.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Translation\LocaleSwitcher;
+
+#[AsEventListener(event: KernelEvents::REQUEST)]
+final readonly class RequestLocaleListener
+{
+    // @todo Solve this with config or detection.
+    private const array ALLOWED_LANGUAGES = ['en', 'nl'];
+
+    public function __construct(private LocaleSwitcher $localeSwitcher)
+    {
+    }
+
+    public static function isAllowed(string $locale): bool
+    {
+        [$language] = explode('-', $locale . '-', 2);
+
+        return in_array($language, self::ALLOWED_LANGUAGES, true);
+    }
+
+    public static function getAcceptLanguages(Request $request): array
+    {
+        $acceptLanguages = array_map(
+            static function (string $spec): array {
+                [$locale, $q] = explode(
+                    ';',
+                    trim($spec) . ';q=1.0',
+                    3
+                );
+
+                return [$locale, (float)substr($q, 2)];
+            },
+            explode(',', $request->headers->get('accept-language'))
+        );
+
+        usort(
+            $acceptLanguages,
+            static function (array $a, array $b): int {
+                [, $qa] = $a;
+                [, $qb] = $b;
+
+                return $qb <=> $qa;
+            }
+        );
+
+        return array_column($acceptLanguages, 0);
+    }
+
+    public function __invoke(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        $session = $request->getSession();
+
+        $acceptLanguages = array_filter(
+            self::getAcceptLanguages($request),
+            self::isAllowed(...)
+        );
+
+        $locale = $request->get('locale')
+            ?? $session->get('_locale')
+            ?? array_shift($acceptLanguages)
+            ?? $this->localeSwitcher->getLocale();
+
+        if (self::isAllowed($locale)) {
+            $this->localeSwitcher->setLocale($locale);
+            $session->set('_locale', $locale);
+        }
+    }
+}
