bcb40ce7bb6326b3824297f40a162c8cb9651479
Jan-Marten de Boer <github@janmarten.name>
2024-01-04 21:29:58 +0100

    Rename Home -> App

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -7,11 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class AppController extends AbstractController
 {
-    #[Route(path: '/', name: 'home')]
+    #[Route(path: '/', name: 'app')]
     public function __invoke(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('app/index.html.twig');
     }
 }
